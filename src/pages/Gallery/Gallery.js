e34173fbf52e687f9c4b0beae1b24969da1d3105
BehinaBhr <be.bhr77@gmail.com>
2024-07-29 16:57:45 -0700

    Initial gallery structure with react-responsive-masonry

---
+++ src/pages/Gallery/Gallery.js
@@ -0,0 +1,30 @@
+import "./Gallery.scss";
+import { DocumentTitle } from "../../utils/utils";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import GalleryElements from "./GalleryElements.js";
+
+function Gallery() {
+  DocumentTitle("Gallery");
+
+  return (
+    <div className="gallery">
+      <h2 className="gallery__title">VPA Gallery</h2>
+      {GalleryElements.map((event) => (
+        <section className="gallery__album">
+          <h3 className="gallery__album-header">
+            {event.date} | {event.info}
+          </h3>
+          <ResponsiveMasonry columnsCountBreakPoints={{ 320: 3, 767: 5, 1279: 7 }}>
+            <Masonry gutter="10px">
+              {event.list.map((image, index) => (
+                <img className="gallery__album-item" key={index} src={image} alt={`${event.info} - ${index}`} loading="lazy" />
+              ))}
+            </Masonry>
+          </ResponsiveMasonry>
+        </section>
+      ))}
+    </div>
+  );
+}
+
+export default Gallery;
